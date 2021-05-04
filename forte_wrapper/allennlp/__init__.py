59ac6b34f399d479d33be2c77a9e91217a15abf4
jingzhi.pang@petuum.com <jingzhi.pang@petuum.com>
2021-05-03 17:43:16 -0700

    fix a pylint

--- forte_wrapper/allennlp/__init__.py
+++ forte_wrapper/allennlp/__init__.py
@@ -1,2 +1,3 @@
-from forte_wrapper.allennlp.allennlp_processors import MODEL2URL, AllenNLPProcessor
+from forte_wrapper.allennlp.allennlp_processors import MODEL2URL, \
+    AllenNLPProcessor
 from forte_wrapper.allennlp.utils_processor import *
