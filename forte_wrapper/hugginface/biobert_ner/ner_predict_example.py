496befc1eb747844d5e7df3d5727c9405ee97e1a
jingzhi.pang@petuum.com <jingzhi.pang@petuum.com>
2021-05-06 09:45:47 -0700

    add huggingface back

--- forte_wrapper/hugginface/biobert_ner/ner_predict_example.py
+++ forte_wrapper/hugginface/biobert_ner/ner_predict_example.py
@@ -23,8 +23,8 @@ from forte.pipeline import Pipeline
 from forte.processors.nltk_processors import NLTKSentenceSegmenter
 from ft.onto.base_ontology import Subword, Sentence, EntityMention
 
-from examples.biobert_ner.transformers_processor import BERTTokenizer
-from examples.biobert_ner.bio_ner_predictor import BioBERTNERPredictor
+from forte_wrapper.hugginface.transformers_processor import BERTTokenizer
+from forte_wrapper.hugginface.bio_ner_predictor import BioBERTNERPredictor
 
 config = yaml.safe_load(open("config.yml", "r"))
 
