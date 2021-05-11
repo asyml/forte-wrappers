893e80d04a50e7cd7c439865488407be8d9b318a
jingzhi.pang@petuum.com <jingzhi.pang@petuum.com>
2021-05-11 15:17:16 -0700

    revise model auto download

--- forte_wrapper/spacy/spacy_processors.py
+++ forte_wrapper/spacy/spacy_processors.py
@@ -87,7 +87,8 @@ class SpacyProcessor(PackProcessor):
 
             # os.system('pip install {}'.format(model))
             # download(self.lang_model)
-            self.nlp = spacy.load(self.lang_model)
+            # self.nlp = spacy.load(self.lang_model)
+            self.nlp = spacy.load(model)
 
             linker = EntityLinker(resolve_abbreviations=True, name="umls")
             self.nlp.add_pipe(linker)
@@ -97,6 +98,60 @@ class SpacyProcessor(PackProcessor):
             except OSError:
                 download(self.lang_model)
                 self.nlp = spacy.load(self.lang_model)
+
+    def run_command(
+            command,
+            *,
+            stdin = None,
+            capture: bool = False,
+    ):
+        """Run a command on the command line as a subprocess. If the subprocess
+        returns a non-zero exit code, a system exit is performed.
+        command (str / List[str]): The command. If provided as a string, the
+            string will be split using shlex.split.
+        stdin (Optional[Any]): stdin to read from or None.
+        capture (bool): Whether to capture the output and errors. If False,
+            the stdout and stderr will not be redirected, and if there's an error,
+            sys.exit will be called with the return code. You should use capture=False
+            when you want to turn over execution to the command, and capture=True
+            when you want to run the command more like a function.
+        RETURNS (Optional[CompletedProcess]): The process object.
+        """
+        if isinstance(command, str):
+            cmd_list = split_command(command)
+            cmd_str = command
+        else:
+            cmd_list = command
+            cmd_str = " ".join(command)
+        try:
+            ret = subprocess.run(
+                cmd_list,
+                env=os.environ.copy(),
+                input=stdin,
+                encoding="utf8",
+                check=False,
+                stdout=subprocess.PIPE if capture else None,
+                stderr=subprocess.STDOUT if capture else None,
+            )
+        except FileNotFoundError:
+            # Indicates the *command* wasn't found, it's an error before the command
+            # is run.
+            raise FileNotFoundError(
+                Errors.E970.format(str_command=cmd_str, tool=cmd_list[0])
+            ) from None
+        if ret.returncode != 0 and capture:
+            message = f"Error running command:\n\n{cmd_str}\n\n"
+            message += f"Subprocess exited with status {ret.returncode}"
+            if ret.stdout is not None:
+                message += f"\n\nProcess log (stdout and stderr):\n\n"
+                message += ret.stdout
+            error = subprocess.SubprocessError(message)
+            error.ret = ret
+            error.command = cmd_str
+            raise error
+        elif ret.returncode != 0:
+            sys.exit(ret.returncode)
+        return ret
 
     # pylint: disable=unused-argument
     def initialize(self, resources: Resources, configs: Config):
