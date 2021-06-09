be1aad91aade274b9a0c6e32c16a1432ddde38d3
hunterhector <hunterhector@gmail.com>
2021-06-09 15:04:47 -0400

    readthedoc

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
-
-import glob
+import sys
 
 # We should be in the `docs` directory, so the VERSION is one step up.
 with open(os.path.join("..", 'VERSION')) as version_file:
