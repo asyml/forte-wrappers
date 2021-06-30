cadc1208bed0d05eaff14d4b6e4ad7c7e0361fd0
hunterhector <hunterhector@gmail.com>
2021-06-30 16:15:04 -0400

    Add pypi setups.

--- src/gpt2/setup.py
+++ src/gpt2/setup.py
@@ -1,6 +1,11 @@
+import os
 import sys
+from pathlib import Path
 
 import setuptools
+
+long_description = (
+        Path(os.path.realpath(__file__)).parent.parent.parent / "README.md").read_text()
 
 if sys.version_info < (3, 6):
     sys.exit("Python>=3.6 is required by Forte.")
@@ -10,7 +15,9 @@ setuptools.setup(
     version="0.0.1",
     url="https://github.com/asyml/forte_wrappers/gpt2",
     description="Provide Forte implementations of a fantastic collection of "
-    "NLP tools.",
+                "NLP tools.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="Apache License Version 2.0",
     packages=["forte.gpt2"],
     include_package_data=True,
