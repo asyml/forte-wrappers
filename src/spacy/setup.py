e8d99021bbda3b7eafa1421aa6464f45aa374cb3
Suqi Sun <suqi.sun@petuum.com>
2021-07-09 13:18:58 -0400

    Fix forte.spacy url

--- src/spacy/setup.py
+++ src/spacy/setup.py
@@ -16,8 +16,8 @@ if sys.version_info < (3, 6):
 
 setuptools.setup(
     name="forte.spacy",
-    version="0.0.1",
-    url="https://github.com/asyml/forte_wrappers/spacy",
+    version="0.0.1.post1",
+    url="https://github.com/asyml/forte-wrappers/tree/main/src/spacy",
     description="Provide Forte implementations of a fantastic collection of "
     "NLP tools.",
     long_description=long_description,
