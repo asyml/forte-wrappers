af72e3160119043a36222d327a5a22ec2066a549
Suqi Sun <suqi.sun@petuum.com>
2021-07-12 13:59:56 -0400

    Address sed issue

--- scripts/update_all_version.sh
+++ scripts/update_all_version.sh
@@ -1,4 +1,8 @@
 for f in src/*/setup.py; do
     echo "Processing $f"
-    sed -r -i "" "s/version=\"[0-9A-Za-z\.]+\",/version=\"$1\",/1" $f
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -r -i "" "s/version=\"[0-9A-Za-z\.]+\",/version=\"$1\",/1" $f
+    else
+        sed -r -i "s/version=\"[0-9A-Za-z\.]+\",/version=\"$1\",/1" $f
+    fi
 done
