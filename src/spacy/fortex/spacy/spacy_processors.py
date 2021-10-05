1cd4dadd0405e485db0d0312efe49fd86fe00ecd
hunterhector <hunterhector@gmail.com>
2021-10-05 11:08:38 -0400

    tweak the spacy parameters

--- src/spacy/fortex/spacy/spacy_processors.py
+++ src/spacy/fortex/spacy/spacy_processors.py
@@ -67,7 +67,7 @@ SPACY3_DEFAULT_CONFIG2COMPONENT = deepcopy(SPACY2_DEFAULT_CONFIG2COMPONENT)
 
 SPACY3_DEFAULT_CONFIG2COMPONENT.update(
     {
-        "sentence": "senter",
+        "sentence": "sentencizer",
         "lemma": "lemmatizer",
     }
 )
@@ -125,7 +125,7 @@ def set_up_pipe(nlp: Language, configs: Config):
 
     # Remove some components to save some time.
     if configs.lang.startswith("en_core_web_sm"):
-        for p in "pos", "ner", "dep", "sentence":
+        for p in "lemma", "pos", "ner", "dep", "sentence":
             if p not in configs.processors:
                 if nlp.has_pipe(config2component[p]):
                     nlp.remove_pipe(config2component[p])
@@ -220,7 +220,11 @@ class SpacyBatchedProcessor(FixedSizeBatchProcessor):
 
     def predict(self, data_batch: Dict) -> Dict[str, List[Any]]:
         return {
-            "results": list(self.nlp.pipe(data_batch["text"]))  # type: ignore
+            "results": list(
+                self.nlp.pipe(  # type: ignore
+                    data_batch["text"], n_process=self.configs.num_processes
+                )
+            )
         }
 
     def pack(
@@ -300,6 +304,9 @@ class SpacyBatchedProcessor(FixedSizeBatchProcessor):
         - `gpu_id`: the GPU device index to use when GPU is enabled. Default
           is 0.
 
+        - `num_processes`: number of processes to run when using `spacy.pipe`.
+          Default is 1. This will be passed directly to the `n_process` option.
+
         """
         return {
             "batcher": {
@@ -309,6 +316,8 @@ class SpacyBatchedProcessor(FixedSizeBatchProcessor):
             "lang": "en_core_web_sm",
             "require_gpu": False,
             "prefer_gpu": False,
+            "gpu_id": 0,
+            "num_processes": 1,
         }
 
 
